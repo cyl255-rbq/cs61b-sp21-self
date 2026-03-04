b0743bf55835f3296051cba3cd3986647125db74
unknown <2550169103@qq.com>
2026-03-04 21:10:15 +0800

    idea3 proj3 1

--- proj3/byow/Core/Rectangle.java
+++ proj3/byow/Core/Rectangle.java
@@ -9,26 +9,11 @@ public class Rectangle {
     private Positon p;
     private int width;
     private int height;
-    private boolean isFull;
-    private Map<Direction, Boolean> edges = new HashMap<>();
 
     Rectangle(Positon p, int width, int height) {
         this.p = p;
         this.width = width;
         this.height = height;
-        this.isFull = false;
-        edges.put(Direction.UP, false);
-        edges.put(Direction.DOWN, false);
-        edges.put(Direction.LEFT, false);
-        edges.put(Direction.RIGHT, false);
-    }
-
-    public void setDirTrue(Direction p) {
-        edges.put(p, true);
-    }
-
-    public Map<Direction, Boolean> edges() {
-        return this.edges;
     }
 
     public int width() {
@@ -41,11 +26,5 @@ public class Rectangle {
 
     public Positon positon() {
         return this.p;
-    }
-
-    public Boolean isFull() {
-        return edges.get(Direction.UP) && edges.get(Direction.DOWN)
-                && edges.get(Direction.LEFT) && edges.get(Direction.RIGHT);
-
     }
 }
--- proj3/byow/Core/WorldGenerator.java
+++ proj3/byow/Core/WorldGenerator.java
@@ -16,7 +16,8 @@ public class WorldGenerator {
     private Random random;
     private TETile[][] world;
     private ArrayList<Rectangle> alternative = new ArrayList<>();
-    private int roomNum;
+    private boolean leftbound = false;
+    private boolean rightbound = false;
 
     WorldGenerator(int width, int height, int seed) {
         this.width = width;
@@ -51,22 +52,21 @@ public class WorldGenerator {
     }
 
     private void fillWithFloor(TETile[][] tiles, Positon p, int width, int height) {
-        //int widthSign = Integer.signum(width);
         int heightSign = Integer.signum(height);
-        //p = p.shiftPosition(widthSign, heightSign);
         for (int i = 0; i < Math.abs(height); i += 1) {
             drawRow(tiles, p, Tileset.FLOOR, width);
             p = p.shiftPosition(0, heightSign);
         }
     }
 
-    public boolean outEdges(Positon p, int width, int height) {
+    private boolean outEdges(Positon p, int width, int height) {
         return ((p.x() + width > WIDTH - 1) || (p.x() + width < 0)) ||
                 ((p.y() + height > HEIGHT - 1) || (p.y() + height < 0)) ||
-                (p.x() == 0 || p.y() == 0);
+                (p.x() == 0 || p.y() == 0) || (p.x() == WIDTH -1)
+                || (p.y() == HEIGHT-1);
     }
 
-    public boolean overlay(TETile[][] tiles, Positon p, int width, int height) {
+    private boolean overlayInside(TETile[][] tiles, Positon p, int width, int height) {
         int x = p.x();
         int y = p.y();
         int widthSign = Integer.signum(width);
@@ -81,100 +81,114 @@ public class WorldGenerator {
         return false;
     }
 
-    public boolean collision(TETile[][] tiles, Positon p, int width, int height) {
-        return outEdges(p, width, height) || overlay(tiles, p, width, height);
+    private boolean inRoomRange(Rectangle room, int x, int y) {
+        Positon p = room.positon();
+        int width = room.width();
+        int height = room.height();
+        int xRange = p.x() + width;
+        int yRange = p.y() + height;
+        boolean xInRange = false;
+        boolean yInRange = false;
+        if (width > 0) {
+            if (p.x() <= x && x < xRange) {
+                xInRange = true;
+            }
+        } else {
+            if (xRange < x && x <= p.x()) {
+                xInRange = true;
+            }
+        }
+        if (height > 0) {
+            if (p.y() <= y && y < yRange) {
+                yInRange = true;
+            }
+        } else {
+            if (yRange < y && y <= p.y()) {
+                yInRange = true;
+            }
+        }
+        return xInRange && yInRange;
+    }
+
+    private boolean overlayOutside(TETile[][] tiles, Positon p, Rectangle father, int width, int height) {
+        int widthSign = Integer.signum(width);
+        int heightSign = Integer.signum(height);
+        p = p.shiftPosition(-widthSign, -heightSign);
+        for (int i = 0; i < Math.abs(width) + 1; i += 1) {
+            int x = p.x() + i * widthSign;
+            int y = p.y() + height + heightSign;
+            if (inRoomRange(father, x, y) || inRoomRange(father, x, p.y())) {
+                continue;
+            }
+            if (tiles[x][y] != Tileset.NOTHING || tiles[x][p.y()] != Tileset.NOTHING) {
+                return true;
+            }
+        }
+        for (int i = 0; i < Math.abs(height) + 1; i += 1) {
+            int y = p.y() + i * heightSign;
+            int x = p.x() + width + widthSign;
+            if (inRoomRange(father, x, y) || inRoomRange(father, p.x(), y)) {
+                continue;
+            }
+            if (tiles[x][y] != Tileset.NOTHING || tiles[p.x()][y] != Tileset.NOTHING) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean collision(TETile[][] tiles, Positon p, Rectangle father, int width, int height) {
+        return outEdges(p, width, height) || overlayInside(tiles, p, width, height)
+                || overlayOutside(tiles, p, father, width, height);
     }
 
     public void addRoomHelper(TETile[][] tiles, Positon p, int width, int height) {
         if (Math.abs(width) < 1 || Math.abs(height) < 1) {
             return;
         }
-
-//        drawRow(tiles, p, Tileset.WALL, width);
-//        drawColumn(tiles, p, Tileset.WALL, height);
-//        drawRow(tiles, p.shiftPosition(0, height - Integer.signum(height)), Tileset.WALL, width);
-//        drawColumn(tiles, p.shiftPosition(width - Integer.signum(width), 0), Tileset.WALL, height);
         fillWithFloor(tiles, p, width, height);
-    }
-
-    public Rectangle addRoomAssist(TETile[][] tiles, Positon p, int widthSign, int heightSign) {
-        int width = RandomUtils.uniform(random, 1, 7) * widthSign;
-        int height = RandomUtils.uniform(random, 1, 7) * heightSign;
-//        width =  3;
-//        height = 3;
-        if (collision(tiles, p, width, height)) {
-            return null;
+        int bound = p.x() + width;
+        if (bound > WIDTH - 3) {
+            this.rightbound = true;
+        } else if (bound < 3) {
+            this.leftbound = true;
         }
-        addRoomHelper(tiles, p, width, height);
-        Rectangle neighbour = new Rectangle(p, width, height);
-        alternative.add(neighbour);
-        return neighbour;
     }
 
-//    public void addRoom(TETile[][] tiles, Positon p) {
-//        int widthSign = RandomUtils.bernoulli(random) ? 1 : -1;
-//        int heightSign = RandomUtils.bernoulli(random) ? 1 : -1;
-//        addRoomAssist(tiles, p, widthSign, heightSign);
-//    }
-
-    public enum Shape {
-        HORIZONTAL, VERTICAL
-    }
-
-    private Shape randomDir() {
-        return RandomUtils.bernoulli(random) ? Shape.HORIZONTAL : Shape.VERTICAL;
-    }
-
-    private void addHallwaysHelper(TETile[][] tiles, Positon p, int length, int anotherLen, Shape dir) {
-        if (Math.abs(length) < 1 || Math.abs(anotherLen) < 1) {
+    public void addRoomAssist(TETile[][] tiles, Positon p, Rectangle room, int widthSign, int heightSign) {
+        int width = RandomUtils.uniform(random, 2, 6) * widthSign;
+        int height = RandomUtils.uniform(random, 2, 6) * heightSign;
+        if (collision(tiles, p, room, width, height)) {
             return;
         }
-        if (dir == Shape.HORIZONTAL) {
-//            drawRow(tiles, p, Tileset.WALL, length);
-//            drawColumn(tiles, p, Tileset.WALL, anotherLen);
-//            drawRow(tiles, p.shiftPosition(0, anotherLen - anotherSign), Tileset.WALL, length);
-//            drawColumn(tiles, p.shiftPosition(length - lengthSign, 0), Tileset.WALL, anotherLen);
-            fillWithFloor(tiles, p, length, anotherLen);
-        }
-        else {
-//            drawRow(tiles, p, Tileset.WALL, anotherLen);
-//            drawColumn(tiles, p, Tileset.WALL, length);
-//            drawRow(tiles, p.shiftPosition(0, length - lengthSign), Tileset.WALL, anotherLen);
-//            drawColumn(tiles, p.shiftPosition(anotherLen - anotherSign, 0), Tileset.WALL, length);
-            fillWithFloor(tiles, p, anotherLen, length);
-        }
+        addRoomHelper(tiles, p, width, height);
+        alternative.add(new Rectangle(p, width, height));
     }
 
-    private boolean hallwaysCollision(TETile[][] tiles, Positon p, int length, int anotherLen, Shape dir) {
-        if (dir == Shape.HORIZONTAL) {
-            return outEdges(p, length, anotherLen) || overlay(tiles, p, length, anotherLen);
-        } else if (dir == Shape.VERTICAL) {
-            return outEdges(p, anotherLen, length) || overlay(tiles, p, anotherLen, length);
-        }
-        return false;
-    }
-
-    public Rectangle addHallwaysAssist(TETile[][] tiles, Positon p, int lengthSign, int anotherSign) {
-        int anotherLen = RandomUtils.uniform(random, 1, 3) * anotherSign;
-        int length = RandomUtils.uniform(random, 1, 13) * lengthSign;
-        Shape dir = randomDir();
-//        anotherLen =  3;
-//        length =  6;
-//        dir = Shape.HORIZONTAL;
-        if (hallwaysCollision(tiles, p, length, anotherLen, dir)) {
-            return null;
-        }
-        addHallwaysHelper(tiles, p, length, anotherLen, dir);
-        Rectangle neighbour = new Rectangle(p, width, height);
-        alternative.add(neighbour);
-        return neighbour;
-    }
-
-    public Rectangle addRectangle(TETile[][] tiles, Positon p, int widthSign, int heightSign) {
+    public void addHallwaysAssist(TETile[][] tiles, Positon p, Rectangle room, int lengthSign, int anotherSign) {
+        int anotherLen = RandomUtils.uniform(random, 1, 2) * anotherSign;
+        int length = RandomUtils.uniform(random, 4, 9) * lengthSign;
+        int width;
+        int height;
         if (RandomUtils.bernoulli(random)) {
-            return addRoomAssist(tiles, p, widthSign, heightSign);
+            width = length;
+            height = anotherLen;
         } else {
-            return addHallwaysAssist(tiles, p, widthSign, heightSign);
+            width = anotherLen;
+            height = length;
+        }
+        if (collision(tiles, p, room, width, height)) {
+            return;
+        }
+        addRoomHelper(tiles, p, width, height);
+        alternative.add(new Rectangle(p, width, height));
+    }
+
+    public void addRectangle(TETile[][] tiles, Positon p,Rectangle room, int widthSign, int heightSign) {
+        if (RandomUtils.bernoulli(random)) {
+            addRoomAssist(tiles, p, room, widthSign, heightSign);
+        } else {
+            addHallwaysAssist(tiles, p, room, widthSign, heightSign);
         }
     }
 
@@ -199,23 +213,6 @@ public class WorldGenerator {
             return RandomUtils.uniform(random, yRange + 1, 1);
         }
     }
-
-    private void bothTrue(Rectangle room, Rectangle neighbour, Direction dir) {
-        if (neighbour == null) {
-            return;
-        }
-        Direction neighbourdir = switch (dir) {
-            case UP -> Direction.DOWN;
-            case DOWN -> Direction.UP;
-            case LEFT -> Direction.RIGHT;
-            case RIGHT -> Direction.LEFT;
-        };
-        room.setDirTrue(dir);
-        neighbour.setDirTrue(neighbourdir);
-        if (room.isFull()) {
-            alternative.remove(room);
-        }
-    }
     
     private void addNeighbour(TETile[][] tiles, Rectangle room, Direction dir) {
         Positon p = room.positon();
@@ -226,7 +223,6 @@ public class WorldGenerator {
         int randomSign = RandomUtils.bernoulli(random) ? 1 : -1;
         int xRange = randomNeighbourX(room);
         int yRange = randomNeighbourY(room);
-        Rectangle neighbour = null;
         switch (dir) {
             case UP -> {
                 if (heightSign > 0) {
@@ -234,9 +230,7 @@ public class WorldGenerator {
                 } else {
                     p = p.shiftPosition(xRange, 1);
                 }
-                //tiles[p.x()][p.y()] = Tileset.SAND;
-                //todo 两个房间添加关系，上面是下面的上，下面是上面的下，应该需要传递Rectangele room 和 Direction dir
-                neighbour = addRectangle(tiles, p, randomSign, 1);
+                addRectangle(tiles, p, room, randomSign, 1);
             }
             case DOWN -> {
                 if (heightSign < 0) {
@@ -244,7 +238,7 @@ public class WorldGenerator {
                 } else {
                     p = p.shiftPosition(xRange, -1);
                 }
-                neighbour = addRectangle(tiles, p, randomSign, -1);
+                addRectangle(tiles, p, room, randomSign, -1);
             }
             case LEFT -> {
                 if (widthSign < 0) {
@@ -252,7 +246,7 @@ public class WorldGenerator {
                 } else {
                     p = p.shiftPosition(-1, yRange);
                 }
-                neighbour = addRectangle(tiles, p, -1, randomSign);
+                addRectangle(tiles, p, room, -1, randomSign);
             }
             case RIGHT -> {
                 if (widthSign > 0) {
@@ -260,12 +254,8 @@ public class WorldGenerator {
                 } else {
                     p = p.shiftPosition(1, yRange);
                 }
-                neighbour = addRectangle(tiles, p, 1, randomSign);
+                addRectangle(tiles, p, room, 1, randomSign);
             }
-        }
-        bothTrue(room, neighbour, dir);
-        if (room.isFull()) {
-            alternative.remove(room);
         }
     }
 
@@ -297,19 +287,36 @@ public class WorldGenerator {
     }
 
     public void extendRectangle(TETile[][] tiles, Rectangle room) {
-        Map<Direction, Boolean> edges = room.edges();
-        for (Direction d : edges.keySet()) {
-            if (!edges.get(d) && RandomUtils.bernoulli(random)) {
-                addNeighbour(tiles, room, d);
+        double probability = 0.5;
+        for (Direction d : Direction.values()) {
+            switch (d) {
+                case LEFT, RIGHT -> {
+                    if (RandomUtils.bernoulli(random, probability)) {
+                        addNeighbour(tiles, room, d);
+                    }
+                }
+                case UP, DOWN -> {
+                    if (RandomUtils.bernoulli(random, 1 - probability)) {
+                        addNeighbour(tiles, room, d);
+                    }
+                }
             }
         }
-//        if (room.checkFull()) {
-//
-//        }
+    }
+
+    private Rectangle randomRectangle() {
+        int size = alternative.size();
+        int index = RandomUtils.uniform(random, 0, size);
+        return alternative.get(index);
+    }
+
+    public void generate(TETile[][] tiles) {
+        while ((!this.leftbound || !this.rightbound) ) {//&& alternative.size() < (WIDTH * HEIGHT / 20)
+            extendRectangle(tiles, randomRectangle());
+        }
     }
 
     public TETile[][] generate() {
-
         //todo
         return world;
     }
@@ -319,11 +326,11 @@ public class WorldGenerator {
         TERenderer ter = new TERenderer();
         ter.initialize(80, 30);
         TETile[][] world = new TETile[80][30];
-        WorldGenerator a = new WorldGenerator(80,30,16344);
+        WorldGenerator a = new WorldGenerator(80,30,1232223);
         a.fillWithNothing(world);
         //Positon p = new Positon(80, 30);
         Positon p0 =new Positon(0,0);
-        Positon p1 = new Positon(1,1);
+        Positon p1 = new Positon(20,15);
         Positon p2 = new Positon(40, 15);
         Positon p3 = new Positon(46,21);
         //范围是1-78
@@ -336,40 +343,10 @@ public class WorldGenerator {
         Positon pHall = new Positon(6,3);
         Positon pHall2 = new Positon(73,26);
 
-        a.addRoomAssist(world,p2, -1, -1);
-//        a.addRoomAssist(world,p4, -1, -1);
-        a.addNeighbour(world, a.alternative.get(0),Direction.LEFT);
-         a.addNeighbour(world, a.alternative.get(0),Direction.RIGHT);
-         a.addNeighbour(world, a.alternative.get(0),Direction.UP);
-         a.addNeighbour(world, a.alternative.get(0),Direction.DOWN);
-//        a.addNeighbour(world, a.alternative.get(1),Direction.LEFT);
-//        a.addNeighbour(world, a.alternative.get(1),Direction.RIGHT);
-//        a.addNeighbour(world, a.alternative.get(1),Direction.UP);
-//        a.addNeighbour(world, a.alternative.get(1),Direction.DOWN);
-        //a.fillWall(world);
-        Map<Direction, Boolean> edge = a.alternative.get(0).edges();
-        for (Direction c : edge.keySet()) {
-            System.out.println(c);
-            System.out.println(edge.get(c));
-        }
-
-        //System.out.println(a.alternative.get(1).isFull());
-        //a.addRoomHelper(world,p2,4,4);
-//        a.addHallwaysHelper(world,p2,6,3,Shape.HORIZONTAL);
-//        a.addHallwaysHelper(world,p2,-6,-3,Shape.HORIZONTAL);
-//        a.addHallways(world,pHall2);
-//        a.addHallways(world,p1);
-//        a.addHallways(world,p2);
-//        a.addHallways(world,p3);
-//        a.addHallways(world,p4);
-//        a.addHallways(world,p5);
-
-//        a.addRoom(world,p2);
-//        a.addRoom(world,p3);
-//        a.addRoom(world,p4);
-//        a.addRoom(world,p1);
-
-
+        a.addRoomHelper(world,p2, -3, -3);
+        a.alternative.add(new Rectangle(p2, -3, -3));
+        a.generate(world);
+        a.fillWall(world);
         ter.renderFrame(world);
     }
 }
