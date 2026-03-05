ee2192ecd9c7c5d8040f83a3672cc1c342f1ae7e
unknown <2550169103@qq.com>
2026-03-05 13:49:34 +0800

    idea3 proj3 1 done fix

--- proj3/byow/Core/Positon.java
+++ proj3/byow/Core/Positon.java
@@ -1,9 +1,32 @@
 package byow.Core;
 
-public record Positon(int x, int y) {
+public class Positon {
+    int x;
+    int y;
+
+    public Positon(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return this.x;
+    }
+
+    public int y() {
+        return this.y;
+    }
 
     public Positon shiftPosition(int x, int y) {
         return new Positon(this.x + x, this.y + y);
     }
 
+//public record Positon(int x, int y) {
+//
+//    public Positon shiftPosition(int x, int y) {
+//        return new Positon(this.x + x, this.y + y);
+//    }
+//
+//}
 }
+
